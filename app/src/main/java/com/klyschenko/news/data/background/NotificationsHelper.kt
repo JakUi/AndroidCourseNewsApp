77560e1e8cf7ae3d69808352356919a01f838825
Konstantin <klyschenko@ourgapps.com>
2025-12-18 12:55:53 +0400

    Pending Intent

--- app/src/main/java/com/klyschenko/news/data/background/NotificationsHelper.kt
+++ app/src/main/java/com/klyschenko/news/data/background/NotificationsHelper.kt
@@ -2,11 +2,14 @@ package com.klyschenko.news.data.background
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.content.getSystemService
 import com.klyschenko.news.R
+import com.klyschenko.news.presentation.MainActivity
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -34,12 +37,25 @@ class NotificationsHelper @Inject constructor(
 
     // отправляет нотификацию
     fun showNewArticlesNotification(topics: List<String>) {
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            PEENDING_INTENT_RC,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE // or используется чтобы указать два флага сразу
+        )
+
         val notification = NotificationCompat.Builder(
             context,
             CHANNEL_ID
         )
             .setSmallIcon(R.drawable.ic_breaking_news) // добавляем иконку оповещения
             .setContentTitle(context.getString(R.string.new_articles_notification_title)) // добавляем заголовок оповещения
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true) // закрывать уведомление после клика на него
             .setContentText(
                 context.getString(
                     R.string.update_subscriptions,
@@ -54,5 +70,6 @@ class NotificationsHelper @Inject constructor(
     companion object {
         private const val CHANNEL_ID = "new_articles"
         private const val NOTIFICATION_ID = 1
+        private const val PEENDING_INTENT_RC = 1
     }
 }
